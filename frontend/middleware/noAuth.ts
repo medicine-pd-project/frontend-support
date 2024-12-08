8f5b7a20efd5cb1ee51ebc75891741b4d8d208ec
Rostislav <rostik057@gmail.com>
2024-12-09 02:59:45 +0300

    !fix auth

---
+++ frontend/middleware/noAuth.ts
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (process.server) return;
+
+  const user = useState("user");
+  if (user.value) return navigateTo("/chats", { external: true });
+});
